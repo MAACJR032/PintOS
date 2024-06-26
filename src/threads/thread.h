96959bb97594bb775c8de7308757cd6e2b0b6390
maacj <maacj@g5c03.cin.ufpe.br>
2024-06-26 09:40:38 -0300

    Início da implementação do nice

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -7,12 +7,12 @@
 
 /* States in a thread's life cycle. */
 enum thread_status
-  {
+{
     THREAD_RUNNING,     /* Running thread. */
     THREAD_READY,       /* Not running but ready to run. */
     THREAD_BLOCKED,     /* Waiting for an event to trigger. */
     THREAD_DYING        /* About to be destroyed. */
-  };
+};
 
 /* Thread identifier type.
    You can redefine this to whatever type you like. */
@@ -87,13 +87,14 @@ struct thread
    enum thread_status status;          /* Thread state. */
    char name[16];                      /* Name (for debugging purposes). */
    uint8_t *stack;                     /* Saved stack pointer. */
+   int64_t tick_to_wake_up;            /* start + sleepTime */
    int priority;                       /* Priority. */
-   struct list_elem allelem;           /* List element for all threads list. */
+   int nice;
 
    /* Shared between thread.c and synch.c. */
    struct list_elem elem;              /* List element. */
+   struct list_elem allelem;           /* List element for all threads list. */
    
-   int64_t tick_to_wake_up; /* start + sleepTime */
 #ifdef USERPROG
    /* Owned by userprog/process.c. */
    uint32_t *pagedir;                  /* Page directory. */
